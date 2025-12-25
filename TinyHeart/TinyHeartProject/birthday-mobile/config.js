be77ebe6e464c2a836e8f1d52d6b590f9e53f151
MinhDucNguyen <nguyenminhduc01032001@gmail.com>
2025-12-26 00:56:50 +0700

    profile của hoàng anh thối

---
+++ TinyHeart/TinyHeartProject/birthday-mobile/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Em rất đáng êuuu đó, em có biết không!',
+    introDesc: ` Ý à! Em đọc được dòng này,em phải mua thuốc đau lưng vì anh làm món quà nhỏ này cho em đó,anh dởn đấy, happy nha em:3 `,
+    btnIntro: 'Em bấm vào đi!',
+    title: 'Anh chân thành muốn trao em một xíu happy ngọt ngào này 🥳',
+    desc: 'Mình là một đôi phải không em:3 ',
+    btnYes: 'Vâng <33',
+    btnNo: 'Em không biết :3',
+    question: ' Em muốn gửi gì cho anh hông hở!',
+    btnReply: 'Gửi luôn anh <3',
+    reply: 'Anh dễ thương và cute hihi <33333333',
+    mess: 'Anh biết em nói vậy mà, anh chân thành cảm ơn em:3',
+    messDesc: 'Bấm vào bên dưới đi em, đừng hồi hộp, anh cũng hồi hộp nữa hihi',
+    btnAccept: 'Bấm nè tin tin <3 ',
+    messLink: 'sinhnhatem-ne-master/happy-birthday-master/index.html'
+}
---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Giáng sinh rồi, em có biết không!',
+    introDesc: `Hoàng anh! em đọc được dòng này,em phải mua thuốc đau lưng vì anh làm món quà nhỏ này cho em đó,anh dởn đấy, happy nha em:3 `,
+    btnIntro: 'em bấm vào đi!',
+    title: 'Anh chân thành muốn trao em một xíu happy ngọt ngào này 🥳',
+    desc: '#',
+    btnYes: 'Vâng <33',
+    btnNo: 'em không biết :3',
+    question: ' em muốn gửi gì cho anh hông!',
+    btnReply: 'Gửi luôn anh <3',
+    reply: 'Anh dễ thương và cute hihi <33333333',
+    mess: 'Anh biết em nói vậy mà, anh chân thành cảm ơn em:3',
+    messDesc: 'Bấm vào bên dưới đi em, đừng hồi hộp, anh cũng hồi hộp nữa hihi',
+    btnAccept: 'Bấm nè tin tin <3 ',
+    messLink: 'sinhnhatem-ne-master/happy-birthday-master/index.html'
+}
